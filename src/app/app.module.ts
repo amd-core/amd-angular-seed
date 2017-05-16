ad7f94b7119c2af5ded7fab2c47a11b2c4975dde
Nathan Mc Grath <contact@nathan-mcgrath.com>
2017-05-16 08:19:10 +0100

    integrate with amd-angular-ui library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AmdButtonModule } from '@amd-core/angular-ui';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  imports: [BrowserModule.withServerTransition({
-    appId: 'amd-angular-seed'
-  })],
+  imports: [
+    BrowserModule.withServerTransition({
+      appId: 'amd-angular-seed'
+    }),
+    AmdButtonModule
+  ],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
